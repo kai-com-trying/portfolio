dfa8d94977356662d1a7b2836731eef8a90c5302
kaicomtrying <172279975+kai-com-trying@users.noreply.github.com>
2024-11-08 16:02:18 +0800

    added basics of section 3

--- app.js
+++ app.js
@@ -34,3 +34,65 @@ secTwoImgContainers.forEach((container) => {
     container.addEventListener('mouseover', overlayAppear);
     container.addEventListener('mouseout', overlayDisappear);
 });
+
+
+/*Section 3*/
+
+let targetSaving = 0;
+let targetYear = 0;
+let interestRate = 0;
+
+const target = document.getElementById('target');
+const targetPlusButton = document.getElementById('target-plus-button');
+const targetMinusButton = document.getElementById('target-minus-button');
+
+const targetSavingYear = document.getElementById('target-year');
+const targetYearPlusButton = document.getElementById('target-year-plus-button');
+const targetYearMinusButton = document.getElementById('target-year-minus-button');
+
+const interest = document.getElementById('interest');
+const interestPlusButton = document.getElementById('interest-plus-button');
+const interestMinusButton = document.getElementById('interest-minus-button');
+
+const saving = document.getElementById('saving');
+
+const addTargetSaving = () => {
+    targetSaving += 10000;
+    target.innerHTML = targetSaving;
+}
+
+const minusTargetSaving = () => {
+    targetSaving -= 10000;
+    target.innerHTML = targetSaving;
+}
+
+const addTargetYear = () => {
+    targetYear ++;
+    targetSavingYear.innerHTML = targetYear;
+}
+
+const minusTargetYear = () => {
+    targetYear --;
+    targetSavingYear.innerHTML = targetYear;
+}
+
+const addInterestRate = () => {
+    interestRate += 0.0025
+    interest.innerHTML = (interestRate * 100).toFixed(2);
+}
+
+const minusInterestRate = () => {
+    interestRate -= 0.0025
+    interest.innerHTML = (interestRate * 100).toFixed(2);
+}
+
+targetPlusButton.addEventListener('click', addTargetSaving);
+targetMinusButton.addEventListener('click', minusTargetSaving);
+
+targetYearPlusButton.addEventListener('click', addTargetYear);
+targetYearMinusButton.addEventListener('click', minusTargetYear);
+
+interestPlusButton.addEventListener('click', addInterestRate);
+interestMinusButton.addEventListener('click', minusInterestRate);
+
+
